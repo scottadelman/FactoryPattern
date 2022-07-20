2a21e030c9c8ff921558696705ba4f0c4d5ce47b
Scott <ScottAdelman97@gmail.com>
2022-07-20 08:30:29 -0600

    Do you want a car or a motorcycle?

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using FactoryPattern;
+
+Console.WriteLine("Lets create a new vehicle! \n");
+
+Console.WriteLine("How many wheels will your new vehicle have?");
+int userInput = Convert.ToInt32(Console.ReadLine());
+var userVehicle = VehicleFactory.GetVehicle(userInput);
+userVehicle.Drive();
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int answer)
+        {
+            if (answer == 2)
+            {
+                return new Motorcycle();
+            }
+            else
+            {
+                return new Car();
+            }
+        }
+    }
+}
